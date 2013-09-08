eb3abbdadff1c260dbe32345cf08d9f0cb1152f0
James Deery <james.deery@jadex.org>
2013-09-08 18:56:10 +0100

    Remove MIDI thread, build new UI

--- src/main.c
+++ src/main.c
@@ -16,49 +16,11 @@
 #include "hamilton/core_synths.h"
 #include "hamilton/cmds.h"
 
-static volatile bool finished = false;
-
-static int midi_thread(void *data)
-{
-	HmBand *band = (HmBand *)data;
-
-	while (!finished) {
-		uint8_t status, data1, data2;
-		while (hm_midi_read(&status, &data1, &data2) > 0) {
-			uint8_t type = status >> 4;
-			uint8_t channel = status & 0x0F;
-
-			switch (type) {
-				case 0x8:
-					hm_band_send_note(band, channel, false, data1, data2 / 127.0);
-					break;
-
-				case 0x9:
-					hm_band_send_note(band, channel, true, data1, data2 / 127.0);
-					break;
-
-				case 0xB:
-					hm_band_send_cc(band, channel, data1, data2 / 127.0);
-					break;
-
-				case 0xC:
-					hm_band_send_patch(band, channel, data1);
-					break;
-			}
-		}
-
-		SDL_Delay(4);
-	}
-
-	return 0;
-}
-
 int main(int argc, char *argv[])
 {
 	BEGIN()
 
 	AlHost *host = NULL;
-	SDL_Thread *midiThread = NULL;
 	HmBand *band = NULL;
 
 	TRY(al_host_systems_init());
@@ -70,19 +32,11 @@ int main(int argc, char *argv[])
 	TRY(hm_audio_init(band));
 
 	hm_load_cmds(al_host_get_lua(host), band);
-
 	TRY(al_host_run_script(host, "main.lua"));
 
 	hm_audio_start();
 
-	midiThread = SDL_CreateThread(midi_thread, band);
-	if (!midiThread)
-		THROW(AL_ERROR_GENERIC);
-
 	al_host_run(host);
-
-	finished = true;
-	SDL_WaitThread(midiThread, NULL);
 
 	PASS(
 		hm_audio_free();
--- src/main.lua
+++ src/main.lua
@@ -6,35 +6,8 @@
 local host = require 'host'
 local hm = require 'hamilton'
 
-local function CC(channel, control)
-	local self = Observable(0)
-	self.changed:add(function(x)
-		hm.send_cc(channel, control, x)
-	end)
-
-	return self
-end
-
 local function Hamilton()
 	local self = {
-		controls = {
-			CC(0,  0),
-			CC(0,  1),
-			CC(0,  2),
-			CC(0,  3),
-			CC(0,  4),
-			CC(0,  5),
-			CC(0,  6),
-			CC(0,  7),
-			CC(0,  8),
-			CC(0,  9),
-			CC(0, 10),
-			CC(0, 11),
-			CC(0, 12),
-			CC(0, 13),
-			CC(0, 14),
-			CC(0, 15)
-		}
 	}
 
 	return self
@@ -44,16 +17,44 @@ local function HamiltonUI(root, app)
 	root:fill_colour(0.2, 0.2, 0.2, 1.0)
 		:bind_down(host.exit)
 
-	for i, cc in ipairs(app.controls) do
-		SliderWidget():add_to(root)
-			:layout(40 * i - 20, nil, nil, 10, nil, 10)
-			:bind_value(cc)
-	end
+	Toolbar():add_to(root)
+		:add_button(0.1, 0.8, 0.3, hm.play)
+		:add_button(0.9, 0.7, 0.2, hm.pause)
+		:layout(10, nil, nil, nil, nil, 10)
 
 	return root
 end
 
 HamiltonUI(Widget.root(), Hamilton())
 
-local synths = {hm.get_synths()}
-hm.set_synth(0, synths[1])
+local synths = hm.get_synths()
+hm.set_synth(1, synths[1])
+hm.set_synth(2, synths[1])
+
+hm.add_note(1, 00000, 2000, 60, 0.5)
+hm.add_note(1, 00020, 1880, 64, 0.5)
+hm.add_note(1, 00040, 1760, 67, 0.5)
+
+hm.add_note(1, 01800, 2200, 67, 0.5)
+hm.add_note(1, 01900, 2100, 64, 0.5)
+hm.add_note(1, 02000, 2000, 60, 0.5)
+
+hm.add_set_patch(2, 0, 32)
+hm.add_note(2, 00000, 200, 65, 0.7)
+hm.add_note(2, 00500, 200, 58, 0.7)
+hm.add_note(2, 01000, 200, 58, 0.7)
+hm.add_note(2, 01500, 200, 58, 0.7)
+
+hm.add_note(2, 02000, 200, 62, 0.7)
+hm.add_note(2, 02500, 200, 58, 0.7)
+hm.add_note(2, 03000, 200, 58, 0.7)
+hm.add_note(2, 03500, 200, 58, 0.7)
+
+hm.seq_commit()
+
+hm.set_loop(00000, 04000)
+hm.set_looping(true)
+
+function frame()
+	local state = hm.get_band_state()
+end
