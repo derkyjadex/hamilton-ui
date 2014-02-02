1f243bb45aaf7781a788abb4c27f5c79eeeb7595
James Deery <james.deery@jadex.org>
2014-02-02 12:27:29 +0000

    Add play head and transport status to UI

--- src/main.lua
+++ src/main.lua
@@ -5,13 +5,7 @@
 
 local host = require 'host'
 local hm = require 'hamilton'
-
-local function Hamilton()
-	local self = {
-	}
-
-	return self
-end
+local commands = require 'commands'
 
 local Icons = {
 	play = Model({
@@ -46,8 +40,170 @@ local Icons = {
 	})
 }
 
+function BandState()
+	local self = {
+		playing = Property(false),
+		position = Property(0),
+		looping = Property(false),
+		loop = Property(0, 0)
+	}
+
+	local playing = Binding(self.playing, function(value)
+		if value then
+			hm.play()
+		else
+			hm.pause()
+		end
+	end)
+
+	local position = Binding(self.position, function(value)
+		hm.seek(value)
+	end)
+
+	local looping = Binding(self.looping, function(value)
+		hm.set_looping(value)
+	end)
+
+	local loop = Binding(self.loop, function(loop_start, loop_end)
+		hm.set_loop(loop_start, loop_end)
+	end)
+
+	local last_state = {}
+	function self.update()
+		local state = hm.get_band_state()
+
+		if state.playing ~= last_state.playing then
+			playing(state.playing)
+		end
+
+		if state.position ~= last_state.position then
+			position(state.position)
+		end
+
+		if state.looping ~= last_state.looping then
+			looping(state.looping)
+		end
+
+		if state.loop_start ~= last_state.loop_start or
+			state.loop_end ~= last_state.loop_end then
+			loop(state.loop_start, state.loop_end)
+		end
+
+		last_state = state
+	end
+
+	return self
+end
+
+PlayHead = Widget:derive(function(self, band_state)
+	Widget.init(self)
+
+	function self:add_to(parent)
+		Widget.prototype.add_to(self, parent)
+
+		local parent_bounds = {parent:bounds()}
+		self:bounds(-3, parent_bounds[2], 3, parent_bounds[4])
+
+		self:bind_property('location', band_state.position, function(position)
+			local parent_bounds = {parent:bounds()}
+			local loop_start, loop_end = band_state:loop()
+
+			local progress = position / (loop_end - loop_start)
+			local x = progress * (parent_bounds[3] - parent_bounds[1])
+
+			return x, parent_bounds[2]
+		end)
+
+		local function seek(x)
+			local parent_bounds = {parent:bounds()}
+			local loop_start, loop_end = band_state:loop()
+
+			x = clamp(x, parent_bounds[1], parent_bounds[3])
+
+			local progress = x / (parent_bounds[3] - parent_bounds[1])
+			local position = progress * (loop_end - loop_start)
+
+			band_state:position(position)
+		end
+
+		local drag_x, drag_y
+
+		parent:bind_down(function(_, x, y)
+			drag_x, drag_y = x, y
+			seek(x)
+			parent:grab_mouse()
+		end)
+
+		parent:bind_motion(function(_, x, y)
+			if x ~= 0 then
+				drag_x = drag_x + x
+				seek(drag_x)
+			end
+		end)
+
+		parent:bind_up(function(_, x, y)
+			parent:release_mouse(drag_x, drag_y)
+		end)
+
+		parent:bind_key(function(_, key)
+			if key == 32 then
+				band_state:playing(not band_state:playing())
+			elseif key == 27 then
+				host.exit()
+			end
+		end)
+
+		return self
+	end
+end)
+
+StateWidget = Widget:derive(function(self, band_state)
+	Widget.init(self)
+
+	self:model_location(15, 15)
+		:text_size(28)
+		:text_location(28, 1.5)
+		:fill_colour(0, 0, 0, 0)
+		:bind_property('model', band_state.playing, function(playing)
+			return playing and Icons.play or Icons.pause
+		end)
+		:bind_property('text', band_state.position, function(position)
+			return string.format('%04d', position)
+		end)
+
+	function self:layout(left, width, right, bottom, height, top)
+		return Widget.prototype.layout(self, left, 80, nil, bottom, 30, nil)
+	end
+end)
+
+local function Hamilton()
+	local self = {
+		band_state = BandState()
+	}
+
+	local on_frame
+	on_frame = function()
+		self.band_state:update()
+		commands.enqueue(on_frame)
+	end
+
+	commands.enqueue(on_frame)
+
+	return self
+end
+
 local function HamiltonUI(root, app)
 	root:fill_colour(0.2, 0.2, 0.2, 1.0)
+		:grid_size(100, 1000)
+		:grid_offset(0, -1)
+		:grid_colour(0.5, 0.5, 0.5)
+
+	StateWidget(app.band_state)
+		:add_to(root)
+		:layout(0, nil, nil, 0, nil, nil)
+
+	PlayHead(app.band_state)
+		:add_to(root)
 
 	Toolbar():add_to(root)
 		:add_button(0.1, 0.8, 0.3, Icons.play, hm.play)
@@ -59,7 +215,8 @@ local function HamiltonUI(root, app)
 	return root
 end
 
-HamiltonUI(Widget.root(), Hamilton())
+local app = Hamilton()
+HamiltonUI(Widget.root(), app)
 
 local synths = hm.get_synths()
 hm.set_synth(1, synths[1])
@@ -86,9 +243,5 @@ hm.add_note(2, 03500, 200, 58, 0.7)
 
 hm.seq_commit()
 
-hm.set_loop(00000, 04000)
-hm.set_looping(true)
-
-function frame()
-	local state = hm.get_band_state()
-end
+app.band_state:loop(00000, 04000)
+app.band_state:looping(true)
